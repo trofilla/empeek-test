5cfe4d9b2762934f212db628e09a95465e98bca7
Aleksandr Trofimenko <trofilla1@gmail.com>
2017-08-18 16:27:34 +0300

    initial project version

---
+++ app/items.service.js
@@ -0,0 +1,47 @@
+app.service('Items', ['$localStorage', function($localStorage) {
+   // $localStorage.$reset();
+  
+   let items = [];
+
+
+   if ($localStorage.savedData) {
+      items = $localStorage.savedData;
+   } 
+   else items = [{
+      name: 'First item with custom name',
+      comments: ['A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s', 
+      'A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s',
+       'A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s']
+   }, {
+      name: 'Second item with custom name',
+      comments: ['A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s',
+       'A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s',
+       'A variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets. It was introduced to the Information Age in the mid-1980s']
+   }, ];
+
+
+   return {
+      setLocalStorage: function() {
+         $localStorage.savedData = items;
+      },
+      addNewItem: function(item) {
+         var singleItem = {
+            name: item,
+            comments: []
+         };
+         items.push(singleItem);
+         this.setLocalStorage();
+      },
+      deleteItem: function(index) {
+         items.splice(index, 1);
+         this.setLocalStorage();
+      },
+      getItems: function() {
+         return items;
+      },
+      newComment: function(text, index) {
+         items[index].comments.push(text);
+         this.setLocalStorage();
+      },
+   };
+}]);
---
+++ app/items/items.controller.js
@@ -0,0 +1,42 @@
+ app.controller('ItemsCtrl', ['$scope', 'Items', function ($scope, Items) {
+        $scope.items = Items.getItems();
+        $scope.IsVisible = false;
+       
+        $scope.addNew = function(newItem){
+        	Items.addNewItem(newItem);
+            // Items.setLocalStorage();
+        };
+
+        $scope.deleteItem = function(item){
+        	var index = $scope.items.indexOf(item);
+        	Items.deleteItem(index);
+        };
+
+        
+        $scope.ShowHide = function (selectedItem, $event) {
+        	var index = $scope.items.indexOf(selectedItem);
+        	$scope.selected = index;
+            let myElement = $event.target;
+            angular.element(myElement).hasClass('sel');
+
+            $scope.IsVisible = ($scope.IsVisible && angular.element(myElement).hasClass('sel'))? false : true;
+            
+            $scope.item = $scope.items[index];
+            return $scope.item;
+        };
+
+        $scope.addComment = function (newComment, item) {
+            var index = $scope.items.indexOf(item);
+            Items.newComment(newComment, index);
+     
+        };
+
+         $scope.saveToLs = function(){
+           // $localStorage.savedData = $scope.items;
+           // console.log($localStorage.savedData); 
+        };
+
+
+    }]);
+
+ 
